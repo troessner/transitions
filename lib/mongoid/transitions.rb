24369bc3175095e1b2cbd887d4cd72e2c9c82e58
Krzysiek Heród <netizer@netizer.pl>
2010-09-17 14:18:42 +0200

    Mongoid support

---
+++ lib/mongoid/transitions.rb
@@ -0,0 +1,56 @@
+# Copyright (c) 2009 Rick Olson
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+module Mongoid
+  module Transitions
+    extend ActiveSupport::Concern
+
+    included do
+      include ::Transitions
+      before_validation :set_initial_state
+      validates_presence_of :state
+      validate :state_inclusion
+    end
+
+    protected
+
+    def write_state(state_machine, state)
+      self.state = state.to_s
+      save!
+    end
+
+    def read_state(state_machine)
+      self.state.to_sym
+    end
+
+    def set_initial_state
+      self.state ||= self.class.state_machine.initial_state.to_s
+    end
+
+    def state_inclusion
+      unless self.class.state_machine.states.map{|s| s.name.to_s }.include?(self.state.to_s)
+        self.errors.add(:state, :inclusion, :value => self.state)
+      end
+    end
+  end
+end
+
