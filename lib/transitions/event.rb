945e856810f15fb98a031ea90fbba10ae631c199
Gavin Morrice <gavin@gavinmorrice.com>
2011-10-03 21:29:16 +0100

    Added tests for timestamp and satisfied tests

--- lib/transitions/event.rb
+++ lib/transitions/event.rb
@@ -22,7 +22,7 @@
 
 module Transitions
   class Event
-    attr_reader :name, :success
+    attr_reader :name, :success, :timestamp
 
     def initialize(machine, name, options = {}, &block)
       @machine, @name, @transitions = machine, name, []
@@ -51,6 +51,8 @@ module Transitions
           break
         end
       end
+      # Update timestamps on obj if a timestamp has been defined
+      update_event_timestamp(obj, next_state) if timestamp_defined?
       next_state
     end
 
@@ -65,14 +67,58 @@ module Transitions
         name == event.name
       end
     end
+    
+    # Has the timestamp option been specified for this event?
+    def timestamp_defined?
+      !@timestamp.nil?
+    end
 
     def update(options = {}, &block)
-      @success = options[:success] if options.key?(:success)
+      @success       = options[:success] if options.key?(:success)
+      self.timestamp = options[:timestamp] if options[:timestamp]
       instance_eval(&block) if block
       self
     end
+    
+    # update the timestamp attribute on obj
+    def update_event_timestamp(obj, next_state)
+      obj.send "#{timestamp_attribute_name(obj, next_state)}=", Time.now
+    end
+    
+    # Set the timestamp attribute.
+    # @raise [ArgumentError] timestamp should be either a String, Symbol or true
+    def timestamp=(value)
+      case value
+      when String, Symbol, TrueClass
+        @timestamp = value
+      else
+        raise ArgumentError, "timestamp must be either: true, a String or a Symbol"
+      end
+    end
+    
 
     private
+    
+    # Returns the name of the timestamp attribute for this event
+    # If the timestamp was simply true it returns the default_timestamp_name
+    # otherwise, returns the user-specified timestamp name
+    def timestamp_attribute_name(obj, next_state)
+      timestamp == true ? default_timestamp_name(obj, next_state) : @timestamp
+    end
+    
+    # If @timestamp is true, try a default timestamp name
+    def default_timestamp_name(obj, next_state)
+      at_name = "%s_at" % next_state
+      on_name = "%s_on" % next_state
+      case
+      when obj.respond_to?(at_name) then at_name
+      when obj.respond_to?(on_name) then on_name
+      else
+        raise NoMethodError, "Couldn't find a suitable timestamp field for event: #{@name}. 
+          Please define #{at_name} or #{on_name} in #{obj.class}"
+      end
+    end
+    
 
     def transitions(trans_opts)
       Array(trans_opts[:from]).each do |s|
