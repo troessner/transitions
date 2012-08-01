e9dba4349d649e1a980356d1cb9d6ac0bead164c
Ben Morris <ben@bnmrrs.com>
2012-08-01 15:35:02 -0400

    Added a helper method to check if we can actually perform a transition Added changelog and readme entries for the new helper methods

---
+++ test/event/test_event_checks.rb
@@ -0,0 +1,34 @@
+require "helper"
+
+class ChecksTestSubject
+  include Transitions
+
+  state_machine :initial => :initial do
+    state :initial
+    state :opened
+    state :closed
+
+    event :open do
+      transitions :from => :initial, :to => :opened
+    end
+
+    event :close do
+      transitions :from => :opened, :to => :closed
+    end
+  end
+end
+
+class StateMachineChecksTest < Test::Unit::TestCase
+  test "checks if a given transition is possible" do
+    subject = ChecksTestSubject.new
+    assert_equal :initial, subject.current_state
+    assert_equal true, subject.can_open?
+    assert_equal false, subject.can_close?
+
+    subject.open
+
+    assert_equal false, subject.can_open?
+    assert_equal true, subject.can_close?
+  end
+end
+
