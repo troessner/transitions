9a4982fee8d0f9e2a7f3e2ea9bf37dba7ada7da8
Timo Rößner <timo.roessner@googlemail.com>
2012-09-25 16:34:47 +0200

    Fix weird and more or less random test failure.

--- test/state_transition/test_state_transition_on_transition_callback.rb
+++ test/state_transition/test_state_transition_on_transition_callback.rb
@@ -1,7 +1,12 @@
 require 'helper'
 
-class Car
+class Truck
   include Transitions
+  attr_reader :test_recorder
+
+  def initialize
+    @test_recorder = []
+  end
 
   state_machine do
     state :parked
@@ -18,26 +23,26 @@ class Car
   end
 
   %w!start_engine loosen_handbrake push_gas_pedal!.each do |m|
-    define_method(m){}
+    define_method(m){ @test_recorder << m }
   end
 end
 
 class TestStateTransitionCallbacks < Test::Unit::TestCase
-  def setup
-    @car = Car.new
-  end
-
   test "should execute callback defined via 'on_transition'" do
-    @car.expects(:start_engine)
-    @car.turn_key!
+    truck = Truck.new
+    truck.expects(:start_engine)
+    truck.turn_key!
   end
 
   test "should execute multiple callbacks defined via 'on_transition' in the same order they were defined" do
-    on_transition_sequence = sequence('on_transition_sequence')
+    # This test requires some explanation: We started out with something like this:
+    #   truck.expects(:start_engine).in_sequence(on_transition_sequence)
+    # Which, after a while (don't ask me why) caused some weird problems and seemed to fail randomly.
+    # Hence the workaround below.
 
-    @car.expects(:start_engine).in_sequence(on_transition_sequence)
-    @car.expects(:loosen_handbrake).in_sequence(on_transition_sequence)
-    @car.expects(:push_gas_pedal).in_sequence(on_transition_sequence)
-    @car.start_driving!
+    truck = Truck.new
+
+    truck.start_driving!
+    assert_equal truck.test_recorder, [:start_engine, :loosen_handbrake, :push_gas_pedal].map(&:to_s)
   end
 end
